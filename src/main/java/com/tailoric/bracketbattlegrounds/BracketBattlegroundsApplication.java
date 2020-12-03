01f9e5ccbba66a7cb309bb1097e90cd4edfe84df
Eric Schneider <eric.schneider96@gmail.com>
2020-12-03 15:22:34 +0100

    Create basic project skeleton

---
+++ src/main/java/com/tailoric/bracketbattlegrounds/BracketBattlegroundsApplication.java
@@ -0,0 +1,13 @@
+package com.tailoric.bracketbattlegrounds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BracketBattlegroundsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BracketBattlegroundsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tailoric/bracketbattlegrounds/BracketBattlegroundsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tailoric.bracketbattlegrounds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BracketBattlegroundsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
